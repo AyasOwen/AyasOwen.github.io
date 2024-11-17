156f3e6d67edfe7ff1495cb9400f54cae6f65d6f
AyasOwen <863237849@qq.com>
2024-11-17 21:26:31 +0800

    Site updated: 2024-11-17 21:26:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/11/hello-world/"];function toRandomPost(){
+var posts=["2024/10/11/hello-world/","2024/11/17/T265和D435的兼容性问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
