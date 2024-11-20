7258183df1265a58582ffc8d915027ad26fc76ef
AyasOwen <863237849@qq.com>
2024-11-20 22:01:17 +0800

    Site updated: 2024-11-20 22:01:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/17/T265和D435的兼容性问题/","2024/11/20/Acfly相关库的安装/","2024/11/20/于-orangepi5-plus-上部署-ros/"];function toRandomPost(){
+var posts=["2024/11/20/Acfly相关库的安装/","2024/11/20/于-orangepi5-plus-上部署-ros/","2024/11/17/T265和D435联合使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
