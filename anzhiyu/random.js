5290ed2d1812745109462269c22733a33e0dbf94
AyasOwen <863237849@qq.com>
2025-01-19 15:02:57 +0800

    Site updated: 2025-01-19 15:02:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/20/Acfly相关库的安装/","2024/11/27/PX4开发之旅/","2024/11/17/T265和D435联合使用/","2025/01/14/Windown上python的环境配置/","2024/11/20/于-orangepi5-plus-上部署-ros1/","2025/01/19/stm32之HAL库开发（一）/","2024/11/27/于-orangepi5-plus-上部署-ros2/"];function toRandomPost(){
+var posts=["2024/11/20/Acfly相关库的安装/","2024/11/17/T265和D435联合使用/","2024/11/27/PX4开发之旅/","2025/01/14/Windown上python的环境配置/","2025/01/19/stm32之HAL库开发（一）/","2024/11/20/于-orangepi5-plus-上部署-ros1/","2024/11/27/于-orangepi5-plus-上部署-ros2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
